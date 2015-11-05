838cff6d81a74a5d0ebad8a453f6b3a997891f2f
Wojciech Naruniec <wojtek@rocketweb.com>
2015-11-05 18:10:10 +0100

    Add initial Platform.sh configs for Magento

---
+++ magento-build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+# Allow to cp dot files easily
+#
+#shopt -s dotglob
+
+#
+# Read read-write directories (Unix)
+#
+#readarray -t  dirs < .platform-read-write-dirs
+
+#
+# Read read-write directories (OS X)
+#
+#while IFS=: read -r dir; do
+#    dirs+=($dir)
+#done < <(grep "" .platform-read-write-dirs)
+
+
+# move files away.
+# loop through array(dirs)
+#for dir in "${dirs[@]}"
+#do
+#    mkdir -p ../init/$dir
+#    cp -R $dir/* ../init/$dir/
+#    rm -rf $dir
+#    mkdir $dir
+#done
